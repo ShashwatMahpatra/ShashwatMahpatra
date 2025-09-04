2a8194d4229dc9b466a6d6b98f4038cdbac8f79b
ShashwatMahpatra <147133918+ShashwatMahpatra@users.noreply.github.com>
2025-09-04 20:07:06 +0530

    Time: 10 ms (99.11%) | Memory: 57.7 MB (68.75%) - LeetSync

--- 1985-maximum-subarray-min-product/maximum-subarray-min-product.java
+++ 1985-maximum-subarray-min-product/maximum-subarray-min-product.java
@@ -1,43 +1,19 @@
 class Solution {
-    public int maxSumMinProduct(int[] nums) {
-        int n = nums.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        long[] prefix = new long[n + 1];
+    public int maxSumMinProduct(int[] a) {
+      int n = a.length, M = 1_000_000_007;
+      long res = 0L;
 
-        // Build prefix sum
-        for (int i = 0; i < n; i++) {
-            prefix[i + 1] = prefix[i] + nums[i];
-        }
-
-        // Find nearest smaller on the left
-        java.util.Deque<Integer> stack = new java.util.ArrayDeque<>();
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && nums[stack.peek()] >= nums[i]) {
-                stack.pop();
-            }
-            left[i] = stack.isEmpty() ? -1 : stack.peek();
-            stack.push(i);
-        }
-
-        // Find nearest smaller on the right
-        stack.clear();
-        for (int i = n - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && nums[stack.peek()] > nums[i]) {
-                stack.pop();
-            }
-            right[i] = stack.isEmpty() ? n : stack.peek();
-            stack.push(i);
-        }
-
-        // Compute max product
-        long maxProduct = 0;
-        for (int i = 0; i < n; i++) {
-            long subarraySum = prefix[right[i]] - prefix[left[i] + 1]; // <-- declare here
-            long product = subarraySum * nums[i];
-            maxProduct = Math.max(maxProduct, product);
-        }
-
-        return (int)(maxProduct % 1_000_000_007);
+      long [] psum = new long[n + 1];
+      for(int i = 0; i < n; i++) psum[i+1] = psum[i] + a[i];
+      int[] s = new int[n];
+      int top = -1;
+      for (int i = 0; i <= n; s[++top] = i++)
+          while (top >= 0 && (i == n || a[i] < a[s[top]])) {
+              int min = a[s[top--]];
+              int l = top == -1 ? 0 : s[top] + 1; 
+              int r = i - 1;
+              res = Math.max(res, min * (psum[r+1] - psum[l]));
+          }
+      return (int) (res % M);
     }
 }
