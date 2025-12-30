69ce9313d7ea41cf9b9a319fb76deb2106f7349f
ShashwatMahpatra <147133918+ShashwatMahpatra@users.noreply.github.com>
2025-12-30 23:32:36 +0530

    Time: 25 ms (12.34%) | Memory: 76.6 MB (93.06%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -3,9 +3,10 @@ class Solution {
         Arrays.sort(nums);
         for(int i=1;i<nums.length;i++){
             if(nums[i]==nums[i-1]){
-                return true;
-            }
-        }
+               return true;
+           }
+       }
         return false;
+
     }
 }
