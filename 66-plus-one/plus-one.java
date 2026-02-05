57a3d4111883aa5b0bbacd0959921e6face51e10
ShashwatMahpatra <147133918+ShashwatMahpatra@users.noreply.github.com>
2026-02-05 23:51:08 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (74.31%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+        int sum=1;
+        for(int i=digits.length-1;i>=0;i--){
+            sum=digits[i] + sum;
+            digits[i]=sum%10;
+            sum=sum/10;
+            if(sum==0){
+                return digits;
+            }
+
+        }
+        int[] newArr = new int[digits.length+1];
+        newArr[0]=1;
+        return newArr;
+    }
+}
