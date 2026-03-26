7f3a4763c29ce5390f203f2e8454a0923c9cbddb
ShashwatMahpatra <147133918+ShashwatMahpatra@users.noreply.github.com>
2026-03-27 00:10:32 +0530

    Time: 35 ms (23.99%) | Memory: 159.9 MB (5.05%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        List<Integer> result = new ArrayList<>();
+        List<Integer> dq=new LinkedList<>();
+        dq.addLast(0);
+        for(int i=1;i<k;i++){
+            while(!dq.isEmpty() && nums[i]>=nums[dq.getLast()]){
+                dq.removeLast();
+
+            }
+            dq.addLast(i);
+        }
+        result.add(nums[dq.getFirst()]);
+        for(int i=k;i<nums.length;i++){
+            if(i-dq.getFirst()>=k){
+                dq.removeFirst();
+            }
+             while(!dq.isEmpty() && nums[i]>=nums[dq.getLast()]){
+                dq.removeLast();
+
+            }
+            dq.addLast(i);
+            result.add(nums[dq.getFirst()]);
+
+        }
+        int[] resArr = new int[result.size()];
+       for (int i = 0; i < result.size(); i++) {
+           resArr[i] = result.get(i);
+       }
+      
+       return resArr;
+
+        
+    }
+}
