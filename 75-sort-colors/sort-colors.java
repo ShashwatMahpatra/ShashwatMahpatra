522a47908b4ba1626baf6b859e171efbbf7e6c4a
ShashwatMahpatra <147133918+ShashwatMahpatra@users.noreply.github.com>
2026-06-17 18:51:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (58.68%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,5 +1,41 @@
+
+
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+         int low =0; //next position for 0
+         int mid=0; //current element
+         int high= nums.length-1; //Next position for 2
+ 
+         while(mid<=high){
+ 
+            //Case 1: Current element is 0
+            if(nums[mid]==0){
+ 
+                //Swap low and mid
+                int temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+ 
+                low++;
+                mid++;
+            }
+ 
+            //Case 2: Current element is 1
+            else if(nums[mid]==1){
+ 
+                //1 is already in current region
+                mid++;
+            }
+ 
+            //CAse 3: Current elemnt is 2
+            else{
+                //Swap mid and high
+                int temp=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+ 
+                high--;
+            }
+         }
     }
 }
