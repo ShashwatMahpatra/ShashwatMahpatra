8e51df71dc58983f54eb21fa5cfbe335bb7c1ac8
ShashwatMahpatra <147133918+ShashwatMahpatra@users.noreply.github.com>
2026-06-22 21:11:23 +0530

    Time: 3 ms (75.53%) | Memory: 46.7 MB (50.24%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -6,16 +6,15 @@ class Solution {
         while(left<right){
              while(left<right && "aeiouAEIOU".indexOf(arr[left])==-1){
                 left++;
-             }
+            }
              while(left<right && "aeiouAEIOU".indexOf(arr[right])==-1){
                 right--;
-             }
+            }
              char temp=arr[left];
              arr[left]=arr[right];
              arr[right]=temp;
              left++;
              right--;
-
         }
         return new String(arr);
     }
